c9e8587d4794ea77a0a2c8f3135a02da447d2260
David Hoppenbrouwers <david@salt-inc.org>
2022-08-04 10:40:24 +0200

    Fix use of google fonts

--- generate.sh
+++ generate.sh
@@ -9,42 +9,5 @@ for file in `find . -name '*.adoc'`; do
     fi
     mkdir -p $(dirname $output)
     echo Generating $output from $file
-    title=$(sed -nr 's:^= (.*):\1:p' $file | head -n 1)
-    if [ -z "$title" ]
-    then
-        title="Norost B"
-    fi
-    asciidoctor $file -o $output
+    asciidoctor -a webfonts! -a stylesheet=theme.css $file -o $output
 done;
-
-exit
-    cat << EOF > $output
-<!DOCTYPE html>
-<head>
-<meta content="text/html;charset=utf-8" http-equiv="Content-Type">
-<title>$title</title>
-<style> 
-html {
-    font-family: sans-serif;
-}
-
-body {
-    width: 768px;
-    margin: 1em auto;
-}
-
-img {
-    width: 100%;
-    margin: 0.4em;
-}
-</style>
-</head>
-<body>
-<header>
-<a href="/">Index</a>
-</header>
-<main>
-`pandoc $file`
-</main>
-</body>
-EOF
